dfcbd02bd08d7f470de8557263a53b1b36c39d90
meghankatea <73602963+meghankatea@users.noreply.github.com>
2023-04-05 16:13:03 -0400

    Added tests for counter

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,63 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+
+import Counter from '../components/Counter.js'
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import userEvent from "@testing-library/user-event";
+
+
 
 beforeEach(() => {
   // Render the Counter component here
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    render(<Counter />);
+
 })
 
+
+
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+    const counterMessage = screen.getByTestId("count");
+    expect(counterMessage).toBeInTheDocument();
+    expect(counterMessage).toHaveTextContent("0");
+
 });
 
-test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+
+
+ test('should render initial count with value of 0', () => {
+     const countElement = screen.getByTestId("count");
+     expect(countElement).toHaveTextContent("0");
+
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+
+ test('clicking + increments the count', () => {
+
+     const incrementButton = screen.getByText("+");
+     const countElement = screen.getByTestId("count");
+
+     fireEvent.click(incrementButton);
+     expect(countElement).toHaveTextContent("1");
+
+     fireEvent.click(incrementButton);
+     expect(countElement).toHaveTextContent("2");
+
+
+
 });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
-});
+ test('clicking - decrements the count', () => {
+
+     const decrementButton = screen.getByText("-");
+     const countElement = screen.getByTestId("count");
+
+     fireEvent.click(decrementButton);
+     expect(countElement).toHaveTextContent("-1");
+
+     fireEvent.click(decrementButton);
+     expect(countElement).toHaveTextContent("-2");
+
+
+ });
+
+
